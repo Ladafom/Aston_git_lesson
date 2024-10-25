5496767fa61b43877fddfc81249aaceb9efad8f0
ladafmn <ladafmn@gmail.com>
2024-10-25 19:19:13 +0700

    feat: homework 2 completed

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
-function helloWorld(phrase){
-  return console.log(phrase)
-}
+//string
+let str1 = 'string with single quotes'
+let str2 = "string with double quotes"
+let strHelper = 3
+let str3 = `i use backtick character ${'!'.repeat(strHelper)}`
+let str4 = new String(true)
 
-helloWorld('Hello!')
+//number
+let num1 = 1.23
+let num2 = Number('Infinity')
+let num = new Number(45)
+
+//boolean
+let bool1 = true
+let bool2 = Boolean(null)
+let bool3 = !!str1
+
+//null
+let n1 = null
+const n2 = null
+
+//undefined
+let und1
+let und2 = undefined
+
+//symbol
+let sym1 = Symbol('foo');
+const sym2 = Symbol('bar');
+
+// bigInt
+let bnum1 = 9007199254740991n
+let bnum2 = BigInt(9007199254740991)
+
+//----------------------------------------
+
+const res = "B" + "a" + (1 - "hello");
+// BaNaN
+console.log(res);
+
+const res2 = (true && 3) + "d";
+// 3d
+console.log(res2);
+
+const res3 = Boolean(true && 3) + "d";
+// trued
+console.log(res3);
